e6d07f249d976222e079a619696dcc3c19c9bb55
florishalleriet <florispshalleriet@gmail.com>
2026-04-19 02:26:22 +0200

    feat: add MaceItemStarterMixin to implement cooldown logic for MaceItem

---
+++ src/main/java/com/flopasss/macecooldown/mixin/MaceItemStarterMixin.java
@@ -0,0 +1,29 @@
+package com.flopasss.macecooldown.mixin;
+
+import net.minecraft.world.item.MaceItem;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+import net.minecraft.world.entity.LivingEntity;
+import net.minecraft.world.entity.player.Player;
+import net.minecraft.world.item.ItemStack;
+
+@Mixin(MaceItem.class)
+public class MaceItemStarterMixin {
+	@Inject(method = "hurtEnemy", at = @At("TAIL"))
+	private void onHurtEnemy(ItemStack stack, LivingEntity target, LivingEntity attacker, CallbackInfo callbackInfo) {
+		// Return if the attacker is not a player
+		if (!(attacker instanceof Player player)) return;
+
+		// Return if the player did not meet the smash requirements
+		if(!(attacker.fallDistance > 1.5 && !attacker.isFallFlying())) return;
+
+		// Return if the mace is already on cooldown
+		ItemStack mace = player.getMainHandItem();
+		if (player.getCooldowns().isOnCooldown(mace)) return;
+
+		// Start the cooldown
+		player.getCooldowns().addCooldown(mace, 600);
+	}
+}
